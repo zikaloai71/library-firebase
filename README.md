b00916ce9cff67dfd08d084beca3fb6041c6dbdc
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-07-21 10:38:07 +0200

    set up react and firebase

---
+++ README.md
@@ -0,0 +1,2 @@
+# description
+library to add your favorite books you plan to read on it and hold the data of added books by firebase.
---
+++ src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {firestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_ftd7dBQo-WFcPaqri_brrpigszDK-DM",
+  authDomain: "fir-library-f51e9.firebaseapp.com",
+  projectId: "fir-library-f51e9",
+  storageBucket: "fir-library-f51e9.appspot.com",
+  messagingSenderId: "848434156658",
+  appId: "1:848434156658:web:5153434f54d85aa7571eeb",
+  measurementId: "G-5FQ1WD7DCD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = firestore(app);
+export default db ;
